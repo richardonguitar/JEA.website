4a0d5082a5b689140016771aff3d84b505d481d7
richardonguitar <richardonguitar@gmail.com>
2024-03-14 18:50:20 +0900

    Added hamburger menu itself with black background

--- homepage.js
+++ homepage.js
@@ -1,7 +1,9 @@
 window.onload = function () {
   const menu_btn = document.querySelector('.hamburger')
+  const navMenu = document.querySelector('.nav-menu')
 
   menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
+    navMenu.classList.toggle('is-active');
   })
 }
